a3778f69a49266d61a59f7214e9c311d684d5943
Josh Espinoza <joshespi@gmial.com>
2019-04-11 16:34:23 -0600

    first upload

---
+++ crest-child/mega-menu-dropdowns/schoolInfo.php
@@ -0,0 +1,42 @@
+<div class="dropDownColumn">
+	<h3><img src="https://canyoncrest.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/calendars.svg" alt="" />Calendars</h3>
+		<ul>
+			<li class="int"><a href="https://canyoncrest.provo.edu/school-information/all-events-calendar/">All Events Calendar</a></li>
+			<li class="int"><a href="https://canyoncrest.provo.edu/school-information/calendar/">Canyon Crest Calendar</a></li>
+			<li class="int"><a href="https://canyoncrest.provo.edu/school-information/district-school-year-calendar/">School Year Calendar</a></li>
+					
+		</ul>
+	<h3><img src="https://canyoncrest.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/parents.svg" alt="" />Parent Resources</h3>
+		<ul>
+			
+			<li class="ext"><a href="https://grades.provo.edu/public/">Grades & Attendance (PowerSchool)</a></li>
+			<li class="int"><a href="https://canyoncrest.provo.edu/faculty-staff/teachers-directory/">Classrooms</a></li>
+			
+		</ul>	
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://canyoncrest.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />About Canyon Crest</h3>
+		<ul>
+			<li class="int"><a href="https://canyoncrest.provo.edu/school-information/contact-our-school/">Contact Our School</a></li>
+            <li class="int"><a href="https://canyoncrest.provo.edu/school-information/school-hours/">Hours of Operation &amp; Daily Schedule</a></li>
+            <li class="pdf"><a href="https://canyoncrest.provo.edu/wp-content/uploads/2019/02/Canyon_Crest-Achievment-Data.pdf">Achievement Data</a></li>
+            <li class="int"><a href="https://canyoncrest.provo.edu/school-information/school-profile/">School Profile</a></li> 
+            <li class="int"><a href="https://canyoncrest.provo.edu/community-council/">Community Council</a></li>
+            <li class="int"><a href="https://provo.edu/transportation/find-your-bus-route/">Bus Routes</a></li>       
+            <li class="int"><a href="https://canyoncrest.provo.edu/school-information/before-after-school/">Before/After School</a></li>  
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://canyoncrest.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-meals.svg" alt="" />School Meals</h3>
+		<ul>
+			<li class="ext"><a href="https://www.myschoolbucks.com/ver2/login/getmain?requestAction=home">Pay for School Meals</a></li>
+            <li class="ext"><a href="https://www.schoollunchapp.com/Terms.aspx?host=provo.schoollunchapp.com&apply=1">Apply for Free and Reduced Meals</a></li>
+            <li class="ext"><a href="http://itsmealsprovo.com/?page=menus">School Menus</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/special-diet-accommodations/">Special Diet Accommodations</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/">Provo Child Nutrition Website</a></li>
+		</ul>
+	<h3><img src="https://canyoncrest.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />Media Center</h3>
+		<ul>
+			<li class="int"><a href="https://canyoncrest.provo.edu/school-information/media-center/">Media Center</a></li>
+		</ul>
+</div>
